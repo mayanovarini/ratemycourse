1091ba73aa9f74c0bc780cc765b78f857ba87bd6
Maya Novarini <Maya@maya-novarini-3.attlocal.net>
2017-05-13 03:16:08 -0700

    add authentication to post a course

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,5 +1,6 @@
 class CoursesController < ApplicationController
   before_action :set_course, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
   # GET /courses
   # GET /courses.json
@@ -26,7 +27,6 @@ class CoursesController < ApplicationController
   # POST /courses.json
   def create
     @course = Course.new(course_params)
-    @course.user_id = current_user.id
 
 
     respond_to do |format|
